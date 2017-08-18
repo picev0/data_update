b4414b46d58249c52b7d328e0071be6ff13f735d
picev0 <ecotech3690s@yahoo.co.jp>
2017-08-19 06:05:55 +0900

    date_update
    自動コミットモードON

--- DataUpdate.java
+++ DataUpdate.java
@@ -69,6 +69,7 @@ public void updateOracle() throws Exception{
 		}catch(SQLException e){
 			
 			// ロールバック
+			//自動コミットがオンの状態でロールバックできないとなる
 			if(conn != null){
 				conn.rollback();
 			}
@@ -76,6 +77,7 @@ public void updateOracle() throws Exception{
 		}catch(Exception e){
 			
 			//ロールバック
+			//自動コミットがオンの状態でロールバックできないとなる
 			if(conn != null){
 				conn.rollback();
 			}
